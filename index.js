fd5453881fd31e306c774a538836cf93b6823f64
Marcio Rosales <marciorosales2005@gmail.com>
2024-11-02 19:50:59 -0300

    Update index.js
    projeto pronto

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+function calcularNivel(vitorias, derrotas) {
 
-saldo = (20 , 12)
+    const saldoVitorias = vitorias - derrotas;
+    
+    let nivel;
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
 
-console.log("seu rank é" + resultado)
-
-function saldo(win, lose){
-    var resultado = win + lose
-    return resultado
+    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível ${nivel}`);
 }
+
+calcularNivel(90, 50 );
